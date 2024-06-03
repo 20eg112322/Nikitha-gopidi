6ef0c73d9eaa36c4dbef30af2c20eb154635b977
Nikitha Gopidi <124144628+20eg112322@users.noreply.github.com>
2024-06-03 15:21:18 +0530

    Update Character_at_odd_position.java

--- Nikitha/2-06-2024/Character_at_odd_position/Character_at_odd_position.java
+++ Nikitha/2-06-2024/Character_at_odd_position/Character_at_odd_position.java
@@ -1,1 +1,17 @@
+package Assign;
 
+import java.util.Scanner;
+
+public class Character_at_odd_position {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String input = sc.nextLine();
+		for (int i = 0; i < input.length(); i++) {
+			if(i%2 != 0) {
+				System.out.print(input.charAt(i));
+			}
+		}
+	}
+
+}
