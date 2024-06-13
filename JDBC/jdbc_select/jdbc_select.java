69ebb7912bd808e439a22a4919aaac1b717ed917
Nikitha Gopidi <124144628+20eg112322@users.noreply.github.com>
2024-06-13 15:51:03 +0530

    Update jdbc_select.java

--- JDBC/jdbc_select/jdbc_select.java
+++ JDBC/jdbc_select/jdbc_select.java
@@ -1,14 +1,14 @@
-package Demo;
+package nikki;
 
 import java.sql.*;
 
-public class JdbcSelect {
+public class jdbc_select{
 	public static void main(String[] args) throws SQLException, ClassNotFoundException {
 		try{
 			Class.forName("com.mysql.cj.jdbc.Driver");
-			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/techm?useSSL=false","root", "Nikki@07");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/demo","root", "Nikki@07");
 			Statement st = con.createStatement();
-			ResultSet rSet = st.executeQuery("select * from customers");
+			ResultSet rSet = st.executeQuery("select * from students");
 			while(rSet.next()) {
 				System.out.println(rSet.getInt(1)+" "+rSet.getString(2)+" "+rSet.getInt(3));
 				
